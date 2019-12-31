279ba904905223ce8cdf3d82c9fc3c0e925c0401
Davide417 <55975436+Davide417@users.noreply.github.com>
2019-12-31 13:50:58 +0100

    Update AbstractUser.java

--- src/logic/Model/AbstractUser.java
+++ src/logic/Model/AbstractUser.java
@@ -1,30 +1,86 @@
 package logic.Model;
 
+import java.util.ArrayList;
+
+import logic.Model.Roles;
+
+
+
 public abstract class AbstractUser {
 	
-
 	protected String name;
 	protected String username;
 	protected String pass;
-	protected int role;
 	protected String mail;
+	private ArrayList<Roles> roles;
+	private Roles type;
+	
+	
+	public AbstractUser() {
+	}
 
+	public abstract Roles getType();
+	
+	public void setType(Roles type) {
+        this.type = type;
+    }
 	
 	
+	public ArrayList<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Roles> roles) {
+        this.roles = roles;
+    }
 	
-	public AbstractUser(String name, String username, String pass, String mail) {
+	
+	 public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
 		this.name = name;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
 		this.username = username;
+	}
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+	public void setPass(String pass) {
 		this.pass = pass;
+	}
+
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
 		this.mail = mail;
-		this.role = 0;
 	}
-	
-	public int getRole() {
-		return this.role;
-	}
-	
-	
+
+
+	public void update(Guest toUpdate) {
+	        this.name = toUpdate.getName();
+	        this.username = toUpdate.getUsername();
+	        this.pass = toUpdate.getPass();
+	        this.mail = toUpdate.getMail();
+	    }
 	
 	
 	

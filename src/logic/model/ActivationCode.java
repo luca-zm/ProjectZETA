b9cfbf7409b71a13a33a99c09cf6658a0272a6fe
Davide417 <55975436+Davide417@users.noreply.github.com>
2020-01-07 20:10:35 +0100

    Aggiunta classe "entity" ActivationCode

---
+++ src/logic/model/ActivationCode.java
@@ -0,0 +1,46 @@
+package logic.model;
+
+public class ActivationCode {
+	
+	private int activationCode;
+	private int greenCoinVal;
+	
+	
+	public ActivationCode(int activationCode, int grenCoinVal) {
+		super();
+		this.activationCode = activationCode;
+		this.greenCoinVal = grenCoinVal;
+	}
+
+
+	public int getActivationCode() {
+		return activationCode;
+	}
+
+
+	public void setActivationCode(int activationCode) {
+		this.activationCode = activationCode;
+	}
+
+
+	public int getGrenCoinVal() {
+		return greenCoinVal;
+	}
+
+
+	public void setGrenCoinVal(int grenCoinVal) {
+		this.greenCoinVal = grenCoinVal;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ActivationCode [activationCode=" + activationCode + ", grenCoinVal=" + greenCoinVal + "]";
+	}
+	
+	
+	
+	
+	
+
+}

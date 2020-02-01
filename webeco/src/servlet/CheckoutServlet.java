6e56070e5cb13ffaf51f92826dfd8268f2c578b8
Davide417 <55975436+Davide417@users.noreply.github.com>
2020-02-01 01:08:33 +0100

    suca cujune

---
+++ webeco/src/servlet/CheckoutServlet.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.ControllerShopCartCheckOut;
+
+/**
+ * Servlet implementation class CheckoutServlet
+ */
+@WebServlet("/CheckoutServlet")
+public class CheckoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	HttpSession session = request.getSession(); 
+
+		String action = request.getParameter("action");
+		
+		ControllerShopCartCheckOut c = new ControllerShopCartCheckOut();
+		
+		if("checkout".contentEquals(action)) {
+			
+			try {
+				c.buyShopCart(session);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}	
+    }
+}
+
---
+++ webeco/src/servlet/ShopCartServlet.java
@@ -0,0 +1,78 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.ControllerShopCartCheckOut;
+import model.Product;
+import persistence.ProductDAO;
+
+/**
+ * Servlet implementation class ShopCart
+ */
+@WebServlet("/ShopCart")
+public class ShopCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShopCartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet1(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			HttpSession session = request.getSession(); 
+
+			String action = request.getParameter("action");
+			ControllerShopCartCheckOut controller = new ControllerShopCartCheckOut();
+			if("del".contentEquals(action)) {
+				int productId = Integer.parseInt(request.getParameter("productId"));
+				try {
+					controller.deleteProduct(productId, session);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	}
+    
+    /**protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	HttpSession session = request.getSession(); 
+    	
+		String action = request.getParameter("action");
+		
+		ArrayList<Product> catalogo = null;
+		try {
+			catalogo = ProductDAO.select();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if("cart".contentEquals(action)) {
+
+		session.setAttribute("catalogo", catalogo);
+		}
+		/**else if("del".contentEquals(action)) {
+			int productId = Integer.parseInt(request.getParameter("productId"));
+			controller.deleteProduct(productId);
+		}**/
+
+
+	
+}

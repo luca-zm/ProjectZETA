4713c7e31ac86db1c1d0fe3123cf2486c48600ba
luca-zm <55975243+luca-zm@users.noreply.github.com>
2020-02-03 18:35:34 +0100

    ManagerClick.java

---
+++ src/logic/ManagerClick.java
@@ -0,0 +1,30 @@
+package logic;
+
+import java.io.IOException;
+
+public class ManagerClick {
+
+	WinNext a = new WinNext();
+	
+	public void goToPath(String event, String id, String style, String label, String path) {
+		
+		if(event.contentEquals("Button[id="+ id +", styleClass="+ style +"]'"+ label +"'")){
+			try {
+				a.openWin(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	public Boolean check(String event, String id, String style, String label) {
+		
+		if(event.contentEquals("Button[id="+ id +", styleClass="+ style +"]'"+ label +"'")){
+			return true;
+		}
+		return false;
+	}
+
+}
